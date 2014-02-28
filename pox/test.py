2e2563aab19f01f666124193688773521e004fc2
Adhip Gupta <adhipgup@usc.edu>
2014-02-27 23:17:18 -0800

    Ping Working

--- pox/test.py
+++ pox/test.py
@@ -13,23 +13,31 @@ class Fast(object):
     def _handle_ConnectionUp(self, event):
         log.info("Adhip: I am up");
         msg = of.ofp_flow_mod()
-        msg.match.nw_dst = IPAddr("10.0.0.1");
-        msg.match.dl_type = 0x800
+        msg.match.dl_type = 0x0806
         msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
         event.connection.send(msg)
-        log.info("Adhip: 1 flow sent");
+        msg = of.ofp_flow_mod()
+        msg.match.nw_dst = IPAddr("10.0.0.1")
+        msg.match.dl_type = 0x0800
+        msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+        event.connection.send(msg)
+        log.info("Adhip: 1 flow sent")
         msg1 = of.ofp_flow_mod()
-        msg1.match.nw_dst = IPAddr("10.0.0.2");
-        msg1.match.dl_type = 0x800
+        msg1.match.nw_dst = IPAddr("10.0.0.2")
+        msg1.match.dl_type = 0x0800
         msg1.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
         event.connection.send(msg1)
         log.info("Adhip: 2 flow sent");
         msg2 = of.ofp_flow_mod()
-        msg2.match.nw_dst = IPAddr("10.0.0.2");
-        msg2.match.dl_type = 0x800
+        msg2.match.nw_dst = IPAddr("10.0.0.3")
+        msg2.match.dl_type = 0x0800
         msg2.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
         event.connection.send(msg2)
-        log.info("Adhip: 3 flow sent");
+        log.info("Adhip: 3 flow sent")
+
+    def _handle_PacketIn (self, event):
+        packet = event.parsed
+        log.info("Packet came in %s %s")
 
 
 
